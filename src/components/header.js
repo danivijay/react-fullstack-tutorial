bfb465e7abeb97956655a29253c8b778162c2728
Dani Vijay <danimvijay@gmail.com>
2018-05-30 22:18:28 +0530

    add styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 
+import '../css/styles.css'
+
 class Header extends Component {
+  
   render () {
     return (
       <header>
-        <div>Logo</div>
+        <div className="logo">Logo</div>
         <input type="text"/>
       </header>
     )
   }
 }
-
 
 export default Header
