60a49d7c933188d8de89574e6aa2b3dd92db2157
Dani Vijay <danimvijay@gmail.com>
2018-06-03 21:52:06 +0530

    init

---
+++ redux/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware()}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
